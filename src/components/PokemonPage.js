4a5fe08cda134ea99cab58ebc10c96a7951c68b7
jackmt9 <jackmt9@gmail.com>
2020-05-16 13:22:28 -0400

    Full Build

--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -5,16 +5,94 @@ import Search from './Search'
 import { Container } from 'semantic-ui-react'
 
 class PokemonPage extends React.Component {
+  
+  state = {
+    pokemonList: [],
+    sortType: "Alphabetically Ascending"
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:3000/pokemon")
+    .then(r => r.json())
+    .then(pokemon => {
+      this.setState({
+        pokemonList: pokemon 
+      })
+    })
+  }
+
+  setSearchParameter = (term) => {
+    console.log(term)
+    this.setState(prevState => ({
+      pokemonList: prevState.pokemonList.filter(pokemon => pokemon.name.includes(term))
+    }))
+  }
+
+  addPokemon = (pokemon) => {
+    this.setState(prevState => ({
+      pokemonList: prevState.pokemonList.concat(pokemon)
+    }))
+  }
+
+  getHP = (pokemon) => {
+    return pokemon.stats.find(sprite => sprite.name === "hp").value
+  }
+  
+  sortedList = () => {
+    let {pokemonList, sortType}= this.state
+    let pokemonListCopy = pokemonList
+
+    switch(sortType) {
+      case "Alphabetically Ascending":
+        pokemonListCopy = pokemonList.sort((a, b) => {return a.name.localeCompare(b.name)})
+        break;
+
+      case "Alphabetically Descending":
+        pokemonListCopy = pokemonList.sort((a, b) => b.name.localeCompare(a.name))
+        break;
+
+      case "Ascending HP":
+        pokemonListCopy = pokemonList.sort((a, b) => 
+          this.getHP(a) - this.getHP(b)
+        )
+        break;
+
+      case "Descending HP":
+        pokemonListCopy = pokemonList.sort((a, b) => 
+        this.getHP(b) - this.getHP(a)
+        )
+        break;
+
+      default:
+        pokemonListCopy = pokemonList
+    }
+    return pokemonListCopy
+  }
+
+  handleSort = (evt) => {
+    this.setState({
+      sortType: evt.target.value
+    })
+  }
+
   render() {
     return (
       <Container>
         <h1>Pokemon Searcher</h1>
         <br />
-        <PokemonForm />
+        <PokemonForm addToPage={this.addPokemon}/>
         <br />
-        <Search onChange={() => console.log('🤔')} />
+        <Search onChange={(evt) => this.setSearchParameter(evt.target.value)} />
         <br />
-        <PokemonCollection />
+        <select value={this.state.sortType} onChange={this.handleSort}>
+          <option value="Alphabetically Ascending">Alphabetically Ascending</option>
+          <option value="Alphabetically Descending">Alphabetically Descending</option>
+          <option value="Ascending HP">Ascending HP</option>
+          <option value="Descending HP">Descending HP</option>
+        </select>
+        <br />
+        <br />
+        <PokemonCollection pokemonList={this.sortedList()}/>
       </Container>
     )
   }
