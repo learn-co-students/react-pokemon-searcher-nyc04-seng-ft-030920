8e7917fba69af417343ead6340979e23e32ed588
isabel k. lee <hello@isabelklee.co>
2020-05-17 17:45:08 -0400

    created ternary operator for image toggle

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -2,17 +2,31 @@ import React from 'react'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCard extends React.Component {
+  state = {
+    toggleImage: false
+  }
+
+  handleToggleImage = (event) => {
+    console.log(event.target, this.state.toggleImage)
+
+    this.setState({
+      toggleImage: !this.state.toggleImage
+    })
+  }
+
   render() {
     
     let { name, sprites, stats } = this.props.pokemon
     let hp = stats.find(stat => stat.name === "hp").value
     let frontImage = sprites["front"]
+    let backImage = sprites["back"]
 
     return (
       <Card>
         <div>
           <div className="image">
-            <img src={frontImage} alt="oh no!" />
+            <img src={frontImage} alt={name} onClick={this.handleToggleImage}/>
+            { this.state.toggleImage ? <img src={frontImage}/> : <img src={backImage}/> }
           </div>
           <div className="content">
           <div className="header">{name}</div>
